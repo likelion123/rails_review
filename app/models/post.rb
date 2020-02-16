b70ade47d4c8464f321256464191837786eb86c2
jomno <jake8955@naver.com>
2020-02-17 02:25:21 +0900

    3.2 callback

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,15 @@ class Post < ApplicationRecord
 
   validates :title, presence: true
   validates :content, length: { maximum: 20 }
+
+  after_create :set_create_comment
+  after_save :set_save_comment
+
+  def set_create_comment
+    self.comments.create(body: 'after create', user: self.user)
+  end
+
+  def set_save_comment
+    comments.create(body: 'after save', user: user)
+  end
 end
