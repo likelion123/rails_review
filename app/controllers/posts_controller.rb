d5307c906bba3966d0483b526a5f8a5d0f56d989
jomno <jake8955@naver.com>
2020-02-16 23:05:13 +0900

    1.2까지 완료

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,49 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+  end
+
+  def create
+    post = Post.new(title: params[:title], content: params[:content], user: current_user)
+
+    if post.save
+      flash[:alert] = "성공적으로 저장했습니다."
+      redirect_to post_path(post)
+    else
+      flash[:error] = "저장을 실패했습니다."
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+
+    if post.update(title: params[:title], content: params[:content])
+      flash[:alert] = "수정을 성공했습니다."
+      redirect_to post_path(post)
+    else
+      flash[:error] = "수정을 실패했습니다."
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+
+    redirect_to posts_path
+  end
+end
